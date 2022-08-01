6cb9855e5bb3e8ddc4fec1b1d032ff45d70e8a4b
Mantas Jurkuvenas <mantasjurkuvenas@gmail.com>
2022-08-01 13:54:40 +0100

    basic exaample added

---
+++ main.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+
+void setup() {
+
+  Wire.begin(); // join i2c bus (address optional for master)
+
+  Serial.begin(9600); // start serial communication at 9600bps
+
+  // move the slicer to the middle
+  Wire.beginTransmission(0xE7); // transmit to device (0xE7)
+  Wire.write(15);               // register to conrol location
+  Wire.write(127);              // go to the middle
+  Wire.endTransmission();       // stop transmitting
+}
+
+void loop() {
+  // now you can move the slider manualy and read where it is
+  Wire.beginTransmission(0xE7); // transmit to device 0xE7
+  Wire.write(17);               // sets register to report location when asked
+  Wire.endTransmission();       // stop transmitting
+
+  Wire.requestFrom(0xE7, 1); // request one bit of data
+  if (Wire.available()) {
+    int readpositions = Wire.read(); // read the position and print
+    Serial.println(readpositions);
+  }
+
+  delay(500);
+}
